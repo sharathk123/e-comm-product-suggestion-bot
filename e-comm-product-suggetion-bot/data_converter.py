eb6adb0e616afcf99bebb64b04ab2fd7d23ad4d9
SharathAIGeek <sharath.babuk@gmail.com>
2024-11-10 18:40:08 +0530

    data_converter

---
+++ e-comm-product-suggetion-bot/data_converter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from langchain.schema import Document
+
+def data_converter():
+    # Load data and select necessary columns
+    product_data = pd.read_csv("data/flipkart_product_review.csv")
+    data = product_data[['product_title', 'review']]
+
+    # Convert DataFrame rows to a list of Document objects
+    docs = [
+        Document(
+            page_content=row["review"],
+            metadata={"product_name": row["product_title"]}
+        )
+        for _, row in data.iterrows()
+    ]
+
+    return docs
